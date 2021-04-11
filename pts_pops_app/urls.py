0c3489c968896f6c07efbdd2c76909f0cda2faf9
Your Name <justindo720@gmail.com>
2021-04-11 06:02:26 -0400

    Created a base.html which has basic navbar and blocks. Started Community Page.

--- pts_pops_app/urls.py
+++ pts_pops_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = 'pts_pops_app'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('community', views.community_page, name='community'),
 ]
