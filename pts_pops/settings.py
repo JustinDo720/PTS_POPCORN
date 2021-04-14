f88f25e07a91e882ff97fda9ad336f6a44247e33
Your Name <justindo720@gmail.com>
2021-04-14 12:25:24 -0400

    Ran collect static and readded SECRET_KEY because heroku wants it

--- pts_pops/settings.py
+++ pts_pops/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-$-(z7f2f-z8poq%_bj616um1j^g6*q8b6-b3dy$)g$xo^n3n9q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,7 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# Static files
+
+STATIC_URL='/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+ os.path.join(BASE_DIR, "/static/"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
